1d0dbf06cedc93688035e6f470a33cba75cbef5c
CanseFr <jucansell@gmail.com>
2023-10-09 17:02:28 +0200

    Fix error testCI

--- shop/tests.py
+++ shop/tests.py
@@ -58,10 +58,9 @@ class TestProduct(ShopAPITestCase):
             {
                 'id': product.pk,
                 'name': product.name,
-                # 'date_created': self.format_datetime(product.date_created),
-                # 'date_updated': self.format_datetime(product.date_updated),
-                # 'category': product.category_id
-                'some error': error
+                'date_created': self.format_datetime(product.date_created),
+                'date_updated': self.format_datetime(product.date_updated),
+                'category': product.category_id
             } for product in products
         ]
 
