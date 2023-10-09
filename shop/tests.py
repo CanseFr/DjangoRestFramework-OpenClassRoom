e2d08bd2007178f023bef3352df00b734830ffc4
CanseFr <jucansell@gmail.com>
2023-10-09 16:57:57 +0200

    Insertion erreur test CI

--- shop/tests.py
+++ shop/tests.py
@@ -58,13 +58,11 @@ class TestProduct(ShopAPITestCase):
             {
                 'id': product.pk,
                 'name': product.name,
-                # 'date_created': self.format_datetime(product.date_created),
-                # 'date_updated': self.format_datetime(product.date_updated),
-                # 'category': product.category_id
-                'some error': error
+                'date_created': self.format_datetime(product.date_created),
+                'date_updated': self.format_datetime(product.date_updated),
+                'category': product.category_id
             } for product in products
         ]
-
 
 
     def test_list(self):
